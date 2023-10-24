21666eeb276cd0199ed7f60845a083eea30f66cf
Jacob Lang <jacoblang72@comcast.net>
2023-10-24 19:21:06 -0400

    made more progress, changed colors, styled a little bit

--- src/components/styles/StyledApp.js
+++ src/components/styles/StyledApp.js
@@ -2,5 +2,9 @@ import styled from "styled-components";
 import '@fontsource/roboto/400.css';
 
 export const StyledApp = styled.div`
-font-family: 'Montserrat', sans-serif !important;
+font-family: 'Playfair Display', serif !important;
+min-height : 100vh;
+height : fit-content;
+background-image: radial-gradient(73% 147%, #EADFDF 59%, #ECE2DF 100%), radial-gradient(91% 146%, rgba(255,255,255,0.50) 47%, rgba(0,0,0,0.50) 100%);
+ background-blend-mode: screen;
 `
